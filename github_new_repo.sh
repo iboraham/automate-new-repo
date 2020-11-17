cc6c203d5d8918b7d36c65051b454e3a5076d2f2
Onur Şerbetçi <data-science-home@users.noreply.github.com>
2020-11-17 03:25:14 +0300

    final update

---
+++ github_new_repo.sh
@@ -0,0 +1,30 @@
+# read repo name
+echo "Please provide repository name"
+read repo_name
+
+# repo desc
+echo "Please provide repository description"
+read desc
+
+echo "What is your username"
+read username
+
+
+mkdir "/Users/onur/Documents/GitHub/${repo_name}"
+cd "/Users/onur/Documents/GitHub/${repo_name}"
+
+# Create repository
+git init
+touch README.MD
+git add README.MD
+git commit -m 'initial automated setup' 
+
+curl -u ${username} https://api.github.com/user/repos -d "{\"name\": \"${repo_name}\", \"description\": \"${desc}\", \"license_template\": \"mit\", \"auto_init\": \"true\"}"
+
+git remote add origin https://github.com/${username}/${repo_name}.git
+git push --set-upstream origin master
+
+cd "/Users/onur/Documents/GitHub/${repo_name}"
+
+echo "Mission Completed"
+echo " *** You're now in your project root. *** "
